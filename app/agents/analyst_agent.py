929f1d1c67b33ff59df08460a0b7ef3a96b99cb1
Jorge Mendez <jorgelmh_01@hotmail.com>
2025-06-21 11:57:12 -0400

    Test RAG implementation without SAFETY MCP tool

--- app/agents/analyst_agent.py
+++ app/agents/analyst_agent.py
@@ -2,7 +2,7 @@ import os
 
 from google.adk.agents import LlmAgent
 
-from app.tools import get_rag_vulnerability_knowledge_tool, get_safety_API_tool
+from app.tools import get_rag_vulnerability_knowledge_tool
 from app.utils.util import load_prompt
 
 analyst_agent = LlmAgent(
@@ -10,6 +10,6 @@ analyst_agent = LlmAgent(
     model=os.environ.get("LLM_DEPLOYMENT", "gemini-2.0-flash"),
     instruction=load_prompt("analyst_agent"),
     description="Analyzes the codebase and identifies vulnerabilities.",
-    tools=[get_safety_API_tool(), get_rag_vulnerability_knowledge_tool()],
+    tools=[get_rag_vulnerability_knowledge_tool()],
     output_key="analysis",
 )
